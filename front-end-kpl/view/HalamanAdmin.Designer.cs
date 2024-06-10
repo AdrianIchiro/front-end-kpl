c9eeb58f475d2080d6f42ed008dc657a2a3e18f0
Rindang Bani Isyan <109974656+Orin10120@users.noreply.github.com>
2024-06-10 07:12:56 +0700

    perbaikan lagi

--- front-end-kpl/view/HalamanAdmin.Designer.cs
+++ front-end-kpl/view/HalamanAdmin.Designer.cs
@@ -47,6 +47,8 @@
             button11 = new Button();
             label6 = new Label();
             button12 = new Button();
+            label7 = new Label();
+            button13 = new Button();
             panel1.SuspendLayout();
             SuspendLayout();
             // 
@@ -54,28 +56,29 @@
             // 
             panel1.BackColor = SystemColors.Highlight;
             panel1.Controls.Add(label1);
-            panel1.Location = new Point(0, 4);
-            panel1.Margin = new Padding(3, 4, 3, 4);
+            panel1.Location = new Point(0, 5);
+            panel1.Margin = new Padding(4, 5, 4, 5);
             panel1.Name = "panel1";
-            panel1.Size = new Size(1537, 133);
+            panel1.Size = new Size(1921, 166);
             panel1.TabIndex = 0;
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(310, 8);
+            label1.Location = new Point(388, 10);
+            label1.Margin = new Padding(4, 0, 4, 0);
             label1.Name = "label1";
-            label1.Size = new Size(379, 81);
+            label1.Size = new Size(453, 96);
             label1.TabIndex = 0;
             label1.Text = "Admin Menu";
             // 
             // button1
             // 
-            button1.Location = new Point(26, 225);
-            button1.Margin = new Padding(3, 4, 3, 4);
+            button1.Location = new Point(32, 281);
+            button1.Margin = new Padding(4, 5, 4, 5);
             button1.Name = "button1";
-            button1.Size = new Size(224, 51);
+            button1.Size = new Size(280, 64);
             button1.TabIndex = 1;
             button1.Text = "Add Admin";
             button1.UseVisualStyleBackColor = true;
@@ -83,10 +86,10 @@
             // 
             // button2
             // 
-            button2.Location = new Point(26, 305);
-            button2.Margin = new Padding(3, 4, 3, 4);
+            button2.Location = new Point(32, 381);
+            button2.Margin = new Padding(4, 5, 4, 5);
             button2.Name = "button2";
-            button2.Size = new Size(224, 51);
+            button2.Size = new Size(280, 64);
             button2.TabIndex = 2;
             button2.Text = "Edit Admin";
             button2.UseVisualStyleBackColor = true;
@@ -94,10 +97,10 @@
             // 
             // button3
             // 
-            button3.Location = new Point(26, 384);
-            button3.Margin = new Padding(3, 4, 3, 4);
+            button3.Location = new Point(32, 480);
+            button3.Margin = new Padding(4, 5, 4, 5);
             button3.Name = "button3";
-            button3.Size = new Size(224, 56);
+            button3.Size = new Size(280, 70);
             button3.TabIndex = 3;
             button3.Text = "Delete Admin";
             button3.UseVisualStyleBackColor = true;
@@ -105,10 +108,10 @@
             // 
             // button4
             // 
-            button4.Location = new Point(310, 225);
-            button4.Margin = new Padding(3, 4, 3, 4);
+            button4.Location = new Point(388, 281);
+            button4.Margin = new Padding(4, 5, 4, 5);
             button4.Name = "button4";
-            button4.Size = new Size(224, 51);
+            button4.Size = new Size(280, 64);
             button4.TabIndex = 4;
             button4.Text = "Add Doctor";
             button4.UseVisualStyleBackColor = true;
@@ -116,10 +119,10 @@
             // 
             // button5
             // 
-            button5.Location = new Point(310, 305);
-            button5.Margin = new Padding(3, 4, 3, 4);
+            button5.Location = new Point(388, 381);
+            button5.Margin = new Padding(4, 5, 4, 5);
             button5.Name = "button5";
-            button5.Size = new Size(224, 51);
+            button5.Size = new Size(280, 64);
             button5.TabIndex = 5;
             button5.Text = "Edit Doctor";
             button5.UseVisualStyleBackColor = true;
@@ -127,10 +130,10 @@
             // 
             // button6
             // 
-            button6.Location = new Point(310, 389);
-            button6.Margin = new Padding(3, 4, 3, 4);
+            button6.Location = new Point(388, 486);
+            button6.Margin = new Padding(4, 5, 4, 5);
             button6.Name = "button6";
-            button6.Size = new Size(224, 51);
+            button6.Size = new Size(280, 64);
             button6.TabIndex = 6;
             button6.Text = "Delete Doctor";
             button6.UseVisualStyleBackColor = true;
@@ -138,10 +141,10 @@
             // 
             // button7
             // 
-            button7.Location = new Point(577, 225);
-            button7.Margin = new Padding(3, 4, 3, 4);
+            button7.Location = new Point(721, 281);
+            button7.Margin = new Padding(4, 5, 4, 5);
             button7.Name = "button7";
-            button7.Size = new Size(224, 51);
+            button7.Size = new Size(280, 64);
             button7.TabIndex = 7;
             button7.Text = "Add Patient";
             button7.UseVisualStyleBackColor = true;
@@ -149,10 +152,10 @@
             // 
             // button8
             // 
-            button8.Location = new Point(577, 387);
-            button8.Margin = new Padding(3, 4, 3, 4);
+            button8.Location = new Point(721, 484);
+            button8.Margin = new Padding(4, 5, 4, 5);
             button8.Name = "button8";
-            button8.Size = new Size(224, 51);
+            button8.Size = new Size(280, 64);
             button8.TabIndex = 8;
             button8.Text = "Edit Patient";
             button8.UseVisualStyleBackColor = true;
@@ -160,10 +163,10 @@
             // 
             // button9
             // 
-            button9.Location = new Point(577, 305);
-            button9.Margin = new Padding(3, 4, 3, 4);
+            button9.Location = new Point(721, 381);
+            button9.Margin = new Padding(4, 5, 4, 5);
             button9.Name = "button9";
-            button9.Size = new Size(224, 51);
+            button9.Size = new Size(280, 64);
             button9.TabIndex = 9;
             button9.Text = "Delete Patient";
             button9.UseVisualStyleBackColor = true;
@@ -171,10 +174,10 @@
             // 
             // button10
             // 
-            button10.Location = new Point(326, 536);
-            button10.Margin = new Padding(3, 4, 3, 4);
+            button10.Location = new Point(408, 670);
+            button10.Margin = new Padding(4, 5, 4, 5);
             button10.Name = "button10";
-            button10.Size = new Size(208, 69);
+            button10.Size = new Size(260, 86);
             button10.TabIndex = 10;
             button10.Text = "Appoiment Management";
             button10.UseVisualStyleBackColor = true;
@@ -184,9 +187,10 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
-            label2.Location = new Point(26, 168);
+            label2.Location = new Point(32, 210);
+            label2.Margin = new Padding(4, 0, 4, 0);
             label2.Name = "label2";
-            label2.Size = new Size(260, 37);
+            label2.Size = new Size(312, 45);
             label2.TabIndex = 1;
             label2.Text = "Admin Management";
             label2.Click += label2_Click;
@@ -195,9 +199,10 @@
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
-            label3.Location = new Point(310, 168);
+            label3.Location = new Point(388, 210);
+            label3.Margin = new Padding(4, 0, 4, 0);
             label3.Name = "label3";
-            label3.Size = new Size(263, 37);
+            label3.Size = new Size(315, 45);
             label3.TabIndex = 11;
             label3.Text = "Doctor Management";
             // 
@@ -205,9 +210,10 @@
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
-            label4.Location = new Point(577, 168);
+            label4.Location = new Point(721, 210);
+            label4.Margin = new Padding(4, 0, 4, 0);
             label4.Name = "label4";
-            label4.Size = new Size(264, 37);
+            label4.Size = new Size(316, 45);
             label4.TabIndex = 12;
             label4.Text = "Patient Management";
             // 
@@ -215,18 +221,19 @@
             // 
             label5.AutoSize = true;
             label5.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
-            label5.Location = new Point(297, 475);
+            label5.Location = new Point(371, 594);
+            label5.Margin = new Padding(4, 0, 4, 0);
             label5.Name = "label5";
-            label5.Size = new Size(315, 37);
+            label5.Size = new Size(378, 45);
             label5.TabIndex = 13;
             label5.Text = "Appoiment Management";
             // 
             // button11
             // 
-            button11.Location = new Point(326, 636);
-            button11.Margin = new Padding(3, 4, 3, 4);
+            button11.Location = new Point(408, 795);
+            button11.Margin = new Padding(4, 5, 4, 5);
             button11.Name = "button11";
-            button11.Size = new Size(208, 69);
+            button11.Size = new Size(260, 86);
             button11.TabIndex = 14;
             button11.Text = "Logout";
             button11.UseVisualStyleBackColor = true;
@@ -236,28 +243,53 @@
             // 
             label6.AutoSize = true;
             label6.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
-            label6.Location = new Point(676, 475);
+            label6.Location = new Point(845, 594);
+            label6.Margin = new Padding(4, 0, 4, 0);
             label6.Name = "label6";
-            label6.Size = new Size(252, 37);
+            label6.Size = new Size(302, 45);
             label6.TabIndex = 15;
             label6.Text = "Room Management";
             label6.Click += label6_Click;
             // 
             // button12
             // 
-            button12.Location = new Point(689, 536);
+            button12.Location = new Point(861, 670);
+            button12.Margin = new Padding(4, 4, 4, 4);
             button12.Name = "button12";
-            button12.Size = new Size(216, 69);
+            button12.Size = new Size(270, 86);
             button12.TabIndex = 16;
             button12.Text = "Room Management";
             button12.UseVisualStyleBackColor = true;
             button12.Click += button12_Click;
             // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            label7.Location = new Point(5, 599);
+            label7.Margin = new Padding(4, 0, 4, 0);
+            label7.Name = "label7";
+            label7.Size = new Size(358, 38);
+            label7.TabIndex = 17;
+            label7.Text = "Specialization Management";
+            // 
+            // button13
+            // 
+            button13.Location = new Point(32, 670);
+            button13.Margin = new Padding(4, 5, 4, 5);
+            button13.Name = "button13";
+            button13.Size = new Size(260, 86);
+            button13.TabIndex = 18;
+            button13.Text = "Specialization Management";
+            button13.UseVisualStyleBackColor = true;
+            // 
             // HalamanAdmin
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(965, 859);
+            ClientSize = new Size(1206, 1050);
+            Controls.Add(button13);
+            Controls.Add(label7);
             Controls.Add(button12);
             Controls.Add(label6);
             Controls.Add(button11);
@@ -276,7 +308,7 @@
             Controls.Add(button2);
             Controls.Add(button1);
             Controls.Add(panel1);
-            Margin = new Padding(3, 4, 3, 4);
+            Margin = new Padding(4, 5, 4, 5);
             Name = "HalamanAdmin";
             Text = "HalamanAdmin";
             panel1.ResumeLayout(false);
@@ -306,5 +338,7 @@
         private Button button11;
         private Label label6;
         private Button button12;
+        private Label label7;
+        private Button button13;
     }
 }
